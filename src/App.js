c84b7d7769eb61e817a07b0f3877a9f67dcbe26b
Akel <akel.hosni@gmail.com>
2021-05-03 10:52:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import image from './img2/imageInSrc.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+   function App() {
+    return (
+      <div>
+        <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+          <h1 className="title red">Akel Hosni</h1>
+          <br />
+          <img  src={image} />
+          <br />
+          <img src={'/img/imageInPublic.jpg'} />
+        </div>
+        <video width={320} height={240} controls>
+          <source src="2 Minute Timer.mp4" />
+        </video>
+      </div>
+    );
+  }
+
+  export default App ;
+
+
